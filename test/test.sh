70a6eb7723361f351e0c847276b175afde9c6d00
Shiming Zhang <wzshiming@foxmail.com>
2022-04-12 17:19:37 +0800

    Add test framework
    Signed-off-by: Shiming Zhang <wzshiming@foxmail.com>

---
+++ test/test.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+TEST_ROOT="$(dirname "${BASH_SOURCE[0]}")"
+
+source "$(dirname "${BASH_SOURCE[0]}")/helper.sh"
+
+function main() {
+    local name
+    local env_name
+    local failed=()
+
+    echo "::group::Build image"
+    build_image
+    echo "::endgroup::"
+
+    for env in "${TEST_ROOT}"/environments/*.env.sh; do
+        [[ -e "${env}" ]] || continue
+        env_name="${env##*/}"
+        env_name="${env_name%.env.sh}"
+        for file in "${TEST_ROOT}"/cases/*.test.sh; do
+            [[ -e "${file}" ]] || continue
+            name="${file##*/}"
+            name="${name%.test.sh}"
+            echo "::group::Running test ${name} on ${env_name}"
+            "${env}" "${file}"
+            if [[ "${?}" -ne 0 ]]; then
+                failed+=("'${name} on ${env_name}'")
+            fi
+            echo "::endgroup::"
+        done
+    done
+
+    if [[ "${#failed[@]}" -gt 0 ]]; then
+        echo "Failed tests: ${failed[*]}"
+        exit 1
+    fi
+}
+
+main
