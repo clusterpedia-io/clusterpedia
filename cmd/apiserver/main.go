8a7129dc1c1da6d3823188726a15373edd3591ca
liangyongzhenya <yongzhen.liang@daocloud.io>
2022-06-14 14:51:30 +0800

    Use k8s.io/component-base/cli to run the command
    Signed-off-by: liangyongzhenya <yongzhen.liang@daocloud.io>
    
    Signed-off-by: liangyongzhenya <yongzhen.liang@daocloud.io>

--- cmd/apiserver/main.go
+++ cmd/apiserver/main.go
@@ -1,29 +1,18 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
 	apiserver "k8s.io/apiserver/pkg/server"
-	"k8s.io/component-base/logs"
+	"k8s.io/component-base/cli"
+	_ "k8s.io/component-base/logs/json/register"
 
 	"github.com/clusterpedia-io/clusterpedia/cmd/apiserver/app"
 )
 
 func main() {
-	if err := runApiServerCommand(); err != nil {
-		fmt.Fprintf(os.Stderr, "%v\n", err)
-		os.Exit(1)
-	}
-}
-
-func runApiServerCommand() error {
-	logs.InitLogs()
-	defer logs.FlushLogs()
-
 	ctx := apiserver.SetupSignalContext()
-	if err := app.NewClusterPediaServerCommand(ctx).Execute(); err != nil {
-		return err
-	}
-	return nil
+	command := app.NewClusterPediaServerCommand(ctx)
+	code := cli.Run(command)
+	os.Exit(code)
 }
--- cmd/clustersynchro-manager/main.go
+++ cmd/clustersynchro-manager/main.go
@@ -1,34 +1,18 @@
 package main
 
 import (
-	"fmt"
-	"math/rand"
 	"os"
-	"time"
 
 	apiserver "k8s.io/apiserver/pkg/server"
-	"k8s.io/component-base/logs"
+	"k8s.io/component-base/cli"
+	_ "k8s.io/component-base/logs/json/register"
 
 	"github.com/clusterpedia-io/clusterpedia/cmd/clustersynchro-manager/app"
 )
 
 func main() {
-	if err := runClusterSynchroManagerCommand(); err != nil {
-		fmt.Fprintf(os.Stderr, "%v\n", err)
-		os.Exit(1)
-	}
-}
-
-func runClusterSynchroManagerCommand() error {
-	rand.Seed(time.Now().UnixNano())
-
-	logs.InitLogs()
-	defer logs.FlushLogs()
-
 	ctx := apiserver.SetupSignalContext()
-	if err := app.NewClusterSynchroManagerCommand(ctx).Execute(); err != nil {
-		return err
-	}
-
-	return nil
+	command := app.NewClusterSynchroManagerCommand(ctx)
+	code := cli.Run(command)
+	os.Exit(code)
 }
