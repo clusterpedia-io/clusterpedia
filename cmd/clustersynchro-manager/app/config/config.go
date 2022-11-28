9349b1abe985e5b2446ec82da316210fad31026e
wlp1153468871 <luping.wei@daocloud.io>
2022-11-28 20:36:07 +0800

    make synchromanager worker configurable
    Signed-off-by: wlp1153468871 <luping.wei@daocloud.io>

--- cmd/clustersynchro-manager/app/config/config.go
+++ cmd/clustersynchro-manager/app/config/config.go
@@ -15,6 +15,7 @@ type Config struct {
 	EventRecorder record.EventRecorder
 
 	StorageFactory storage.StorageFactory
+	WorkerNumber   int
 
 	LeaderElection   componentbaseconfig.LeaderElectionConfiguration
 	ClientConnection componentbaseconfig.ClientConnectionConfiguration
